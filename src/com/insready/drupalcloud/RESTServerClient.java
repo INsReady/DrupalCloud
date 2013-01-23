36d1d50aabbb35b9687d4ad25bcd5fd61e01ce46
Jingsheng Wang <skyred@gmail.com>
2013-01-23 17:45:26 +0800

    Following Android Code Style Guidelines for Contributors -- Don't Ignore Exceptions -- http://source.android.com/source/code-style.html#dont-ignore-exceptions
    , Throw the exception up to the caller of your method.

--- src/com/insready/drupalcloud/RESTServerClient.java
+++ src/com/insready/drupalcloud/RESTServerClient.java
@@ -3,7 +3,6 @@ package com.insready.drupalcloud;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,8 +29,8 @@ public class RESTServerClient {
 	private String mSession = null;
 	public static Long mSESSION_LIFETIME;
 
-	public RESTServerClient(String _server,
-			String _endpoint, Long _session_lifetime) {
+	public RESTServerClient(String _server, String _endpoint,
+			Long _session_lifetime) {
 		mENDPOIN = _server + _endpoint;
 		mSESSION_LIFETIME = _session_lifetime;
 	}
@@ -46,100 +45,76 @@ public class RESTServerClient {
 
 	public InputStreamReader callPost(String url,
 			BasicNameValuePair[] parameters)
-			throws ServiceNotAvailableException {
+			throws ServiceNotAvailableException, ClientProtocolException,
+			IOException {
 		mSERVERPOST = new HttpPost(url);
 		mPairs.clear();
-		try {
-			if (getSession() != null) {
-				mSERVERPOST.setHeader("Cookie", getSession());
-			}
-			for (int i = 0; i < parameters.length; i++) {
-				mPairs.add(parameters[i]);
-			}
-			mSERVERPOST.setEntity(new UrlEncodedFormEntity(mPairs));
-			HttpResponse response = mClient.execute(mSERVERPOST);
-			InputStream is = response.getEntity().getContent();
-			InputStreamReader isr = new InputStreamReader(is, "UTF-8");
-			return isr;
-
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+		if (getSession() != null) {
+			mSERVERPOST.setHeader("Cookie", getSession());
 		}
-		return null;
+		for (int i = 0; i < parameters.length; i++) {
+			mPairs.add(parameters[i]);
+		}
+		mSERVERPOST.setEntity(new UrlEncodedFormEntity(mPairs));
+		HttpResponse response = mClient.execute(mSERVERPOST);
+		InputStream is = response.getEntity().getContent();
+		InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+		return isr;
+
 	}
 
 	public InputStreamReader callGet(String url)
-			throws ServiceNotAvailableException {
+			throws ServiceNotAvailableException, ClientProtocolException,
+			IOException {
 		mSERVERGET = new HttpGet(url);
 
-		try {
-			mSERVERGET.setHeader("Cookie", getSession());
-			HttpResponse response = mClient.execute(mSERVERGET);
-			InputStream is = response.getEntity().getContent();
-			InputStreamReader isr = new InputStreamReader(is, "UTF-8");
-			return isr;
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
+		mSERVERGET.setHeader("Cookie", getSession());
+		HttpResponse response = mClient.execute(mSERVERGET);
+		InputStream is = response.getEntity().getContent();
+		InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+		return isr;
+
 	}
 
-/*	private void systemConnect() throws ServiceNotAvailableException {
-		// Cloud server hand shake
-		String uri = mENDPOIN + "system/connect";
-		mSERVERPOST = new HttpPost(uri);
-		try {
-			HttpResponse response = mClient.execute(mSERVERPOST);
-			InputStream result = response.getEntity().getContent();
-			BufferedReader br = new BufferedReader(
-					new InputStreamReader(result));
-			String tmp = br.readLine();
-			if (tmp == null) {
-				// TODO throw new ServiceNotAvailableException(this,
-				// "Invalid method");
-			}
-			JSONObject jso = new JSONObject(tmp);
-			// Save the sessionid to storage
-			SharedPreferences auth = mCtx.getSharedPreferences(mPREFS_AUTH, 0);
-			SharedPreferences.Editor editor = auth.edit();
-			editor.putString("sessionid", jso.getString("sessid"));
-			editor.putLong("sessionid_timestamp", new Date().getTime() / 100);
-			editor.commit();
-
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}*/
+	/*
+	 * private void systemConnect() throws ServiceNotAvailableException { //
+	 * Cloud server hand shake String uri = mENDPOIN + "system/connect";
+	 * mSERVERPOST = new HttpPost(uri); try { HttpResponse response =
+	 * mClient.execute(mSERVERPOST); InputStream result =
+	 * response.getEntity().getContent(); BufferedReader br = new
+	 * BufferedReader( new InputStreamReader(result)); String tmp =
+	 * br.readLine(); if (tmp == null) { // TODO throw new
+	 * ServiceNotAvailableException(this, // "Invalid method"); } JSONObject jso
+	 * = new JSONObject(tmp); // Save the sessionid to storage SharedPreferences
+	 * auth = mCtx.getSharedPreferences(mPREFS_AUTH, 0);
+	 * SharedPreferences.Editor editor = auth.edit();
+	 * editor.putString("sessionid", jso.getString("sessid"));
+	 * editor.putLong("sessionid_timestamp", new Date().getTime() / 100);
+	 * editor.commit();
+	 * 
+	 * } catch (UnsupportedEncodingException e) { e.printStackTrace(); } catch
+	 * (ClientProtocolException e) { e.printStackTrace(); } catch (IOException
+	 * e) { e.printStackTrace(); } catch (JSONException e) { // TODO
+	 * Auto-generated catch block e.printStackTrace(); } }
+	 */
 
 	public JsonReader nodeGet(int nid, String fields)
-			throws ServiceNotAvailableException {
+			throws ServiceNotAvailableException, ClientProtocolException,
+			IOException {
 		String uri = mENDPOIN + "node/" + nid;
 		JsonReader jsr = new JsonReader(callGet(uri));
 		return jsr;
 	}
 
 	public JsonReader taxonomyVocabGetTree(int vid)
-			throws ServiceNotAvailableException {
+			throws ServiceNotAvailableException, ClientProtocolException,
+			IOException {
 		return taxonomyVocabGetTree(vid, 0, 0);
 	}
 
 	public JsonReader taxonomyVocabGetTree(int vid, int parent, int maxdepth)
-			throws ServiceNotAvailableException {
+			throws ServiceNotAvailableException, ClientProtocolException,
+			IOException {
 		String uri = mENDPOIN + "taxonomy_vocabulary/getTree";
 		int size = 2;
 		BasicNameValuePair[] parameters = null;
@@ -174,53 +149,37 @@ public class RESTServerClient {
 		return null;
 	}
 
-/*	public boolean flagFlag(String flagName, int contentId, int uid,
-			boolean action, boolean skipPermissionCheck)
-			throws ServiceNotAvailableException {
-		String uri = mENDPOIN + "flag/flag";
-		BasicNameValuePair[] parameters = new BasicNameValuePair[5];
-		parameters[0] = new BasicNameValuePair("flag_name", flagName);
-		parameters[1] = new BasicNameValuePair("content_id",
-				String.valueOf(contentId));
-		parameters[2] = new BasicNameValuePair("uid", String.valueOf(uid));
-		String actionName = (action) ? "flag" : "unflag";
-		parameters[3] = new BasicNameValuePair("action", actionName);
-		String skipPermissionCheckName = (skipPermissionCheck) ? "TRUE"
-				: "FALSE";
-		parameters[4] = new BasicNameValuePair("skip_permission_check",
-				skipPermissionCheckName);
-		//String result = call(uri, parameters);
-		JSONObject jso;
-		try {
-			jso = new JSONObject(result);
-			boolean flag = jso.getBoolean("#data");
-			return flag;
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
-		return false;
-	}*/
+	/*
+	 * public boolean flagFlag(String flagName, int contentId, int uid, boolean
+	 * action, boolean skipPermissionCheck) throws ServiceNotAvailableException
+	 * { String uri = mENDPOIN + "flag/flag"; BasicNameValuePair[] parameters =
+	 * new BasicNameValuePair[5]; parameters[0] = new
+	 * BasicNameValuePair("flag_name", flagName); parameters[1] = new
+	 * BasicNameValuePair("content_id", String.valueOf(contentId));
+	 * parameters[2] = new BasicNameValuePair("uid", String.valueOf(uid));
+	 * String actionName = (action) ? "flag" : "unflag"; parameters[3] = new
+	 * BasicNameValuePair("action", actionName); String skipPermissionCheckName
+	 * = (skipPermissionCheck) ? "TRUE" : "FALSE"; parameters[4] = new
+	 * BasicNameValuePair("skip_permission_check", skipPermissionCheckName);
+	 * //String result = call(uri, parameters); JSONObject jso; try { jso = new
+	 * JSONObject(result); boolean flag = jso.getBoolean("#data"); return flag;
+	 * } catch (JSONException e) { e.printStackTrace(); } return false; }
+	 */
 
-/*	public boolean flagIsFlagged(String flagName, int contentId, int uid)
-			throws ServiceNotAvailableException {
-		String uri = mENDPOIN + "flag/flag_isflaged";
-		BasicNameValuePair[] parameters = new BasicNameValuePair[3];
-		parameters[0] = new BasicNameValuePair("flag_name", flagName);
-		parameters[1] = new BasicNameValuePair("content_id",
-				String.valueOf(contentId));
-		parameters[2] = new BasicNameValuePair("uid", String.valueOf(uid));
-		String result = call(uri, parameters);
-
-		JSONObject jso;
-		try {
-			jso = new JSONObject(result);
-			boolean flag = jso.getBoolean("");
-			return flag;
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
-		return false;
-	}*/
+	/*
+	 * public boolean flagIsFlagged(String flagName, int contentId, int uid)
+	 * throws ServiceNotAvailableException { String uri = mENDPOIN +
+	 * "flag/flag_isflaged"; BasicNameValuePair[] parameters = new
+	 * BasicNameValuePair[3]; parameters[0] = new
+	 * BasicNameValuePair("flag_name", flagName); parameters[1] = new
+	 * BasicNameValuePair("content_id", String.valueOf(contentId));
+	 * parameters[2] = new BasicNameValuePair("uid", String.valueOf(uid));
+	 * String result = call(uri, parameters);
+	 * 
+	 * JSONObject jso; try { jso = new JSONObject(result); boolean flag =
+	 * jso.getBoolean(""); return flag; } catch (JSONException e) {
+	 * e.printStackTrace(); } return false; }
+	 */
 
 	public JsonReader userLogin(String username, String password)
 			throws ServiceNotAvailableException, IOException {
@@ -233,18 +192,18 @@ public class RESTServerClient {
 		return jsr;
 	}
 
-/*	public String userLogout(String sessionID)
-			throws ServiceNotAvailableException {
-		// TODO Auto-generated method stub
-		BasicNameValuePair[] parameters = new BasicNameValuePair[0];
-		// parameters[0] = new BasicNameValuePair("sessid", sessionID);
-		String uri = mENDPOIN + "user/logout";
-		return call(uri, parameters);
-	}*/
+	/*
+	 * public String userLogout(String sessionID) throws
+	 * ServiceNotAvailableException { // TODO Auto-generated method stub
+	 * BasicNameValuePair[] parameters = new BasicNameValuePair[0]; //
+	 * parameters[0] = new BasicNameValuePair("sessid", sessionID); String uri =
+	 * mENDPOIN + "user/logout"; return call(uri, parameters); }
+	 */
 
 	public JsonReader viewsGet(String view_name, String display_id,
 			String args, int offset, int limit)
-			throws ServiceNotAvailableException {
+			throws ServiceNotAvailableException, ClientProtocolException,
+			IOException {
 		String uri = mENDPOIN + "views/" + view_name + "?" + "limit=" + limit
 				+ "&offset=" + offset;
 
